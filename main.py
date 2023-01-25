aa1c5946043d5a0886cb7a920345bc4c0912e342
tony <twang35@outlook.com>
2023-01-24 23:10:17 -0800

    Car drives forward

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
 
+from car_racing import CarRacing
 
-if __name__ == '__main__':
-    print_hi('Formula Fun')
+game = CarRacing(render_mode="human")
+game.reset()
+
+for i in range(300):
+    action = np.array([0, +1.0, 0])
+    game.step(action)
+    game.render()
+
 
